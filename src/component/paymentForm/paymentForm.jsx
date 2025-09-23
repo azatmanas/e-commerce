2664d6d7305494ba0a0ceeff329803bab2737f44
AZAT MANAS <ZTMANAS@GMAIL.COM>
2025-09-23 16:29:02 -0400

    cart action tsx to jsx file

--- src/component/paymentForm/paymentForm.jsx
+++ src/component/paymentForm/paymentForm.jsx
@@ -1,13 +1,8 @@
 import { useState } from "react";
 import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
-
-import { StripeCardElement } from "@stripe/stripe-js";
-
 import { useSelector } from "react-redux";
-
 import { selectCartTotal } from "../../store/cart/cart.selector";
 import { selectCurrentUser } from "../../store/user/user.selector";
-
 import { BUTTON_TYPE_CLASSES } from "../button/button";
 import {
   PaymentFormContainer,
