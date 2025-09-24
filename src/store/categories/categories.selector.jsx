faefd395037713ce7dfd6c1c83773e29e48139ef
AZAT MANAS <ZTMANAS@GMAIL.COM>
2025-09-24 00:06:55 -0400

    selector ts to jsx

--- src/store/categories/categories.selector.jsx
+++ src/store/categories/categories.selector.jsx
@@ -1,13 +1,5 @@
 import { createSelector } from "reselect";
-
-import { RootState } from "../store";
-
-import { CategoriesState } from "./categories.reducer";
-
-import { CategoryMap } from "./categories.types";
-
-const selectCategoryReducer = (state: RootState): CategoriesState =>
-  state.categories;
+const selectCategoryReducer = (state) => state.categories;
 
 export const selectCategories = createSelector(
   [selectCategoryReducer],
@@ -16,12 +8,12 @@ export const selectCategories = createSelector(
 
 export const selectCategoriesMap = createSelector(
   [selectCategories],
-  (categories): CategoryMap =>
+  (categories) =>
     categories.reduce((acc, category) => {
       const { title, items } = category;
       acc[title.toLowerCase()] = items;
       return acc;
-    }, {} as CategoryMap)
+    }, {})
 );
 
 export const selectCategoriesIsLoading = createSelector(
